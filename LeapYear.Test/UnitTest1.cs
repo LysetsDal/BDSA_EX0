6aa4680d9b149e3b71065343205bffec93d9e877
asger1278 <asly@itu.dk>
2022-09-04 16:00:49 +0200

    Made helloworld unit test

--- LeapYear.Test/UnitTest1.cs
+++ LeapYear.Test/UnitTest1.cs
@@ -5,7 +5,13 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        // Arrange
+        Program ly = new Program();
 
-        
+        // Act
+        var temp = ly.HelloWorld();
+        // Assert
+        temp.Should().Be("Hello World!");
+
     }
 }
